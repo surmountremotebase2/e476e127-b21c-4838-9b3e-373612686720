37f74dff16af7d444eb1e6ba46ea54c47128dfd9
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-03-29 12:16:08 -0400

    User Hamza EL MEKKOUDI saved changes to main.py at 03/29/2024, 16:16:07 UTC time.

--- db81eab1-2c8f-49f9-ac5f-15de39562663/main.py
+++ db81eab1-2c8f-49f9-ac5f-15de39562663/main.py
@@ -1,1 +1,33 @@
-#Type code here
+from surmount.base_class import Strategy, TargetAllocation
+from surmount.technical_indicators import RSI, EMA, SMA, MACD, MFI, BB
+from surmount.logging import log
+from surmount.data import Asset, InstitutionalOwnership, InsiderTrading
+
+class TradingStrategy(Strategy):
+
+    def __init__(self):
+        self.tickers = ["SPY", "QQQ", "AAPL", "GOOGL"]
+        self.data_list = [InstitutionalOwnership(i) for i in self.tickers]
+        self.data_list += [InsiderTrading(i) for i in self.tickers]
+
+    @property
+    def interval(self):
+        return "1day"
+
+    @property
+    def assets(self):
+        return self.tickers
+
+    @property
+    def data(self):
+        return self.data_list
+
+    def run(self, data):
+        allocation_dict = {i: 1/len(self.tickers) for i in self.tickers}
+        for i in self.data_list:
+            if tuple(i)[0]=="insider_trading":
+                if data[tuple(i)] and len(data[tuple(i)])>0:
+                if "Sale" in data[tuple(i)][-1]['transactionType']:
+                    allocation_dict[tuple(i)[1]] = 0
+
+        return TargetAllocation(allocation_dict)
